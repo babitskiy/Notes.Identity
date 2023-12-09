f7740180cf36f31215c9276860c5554e977290d8
john <babickii_@mail.ru>
2023-12-09 17:15:01 +0300

    first commit

---
+++ Notes.Identity/Data/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Notes.Identity.Data
+{
+	public class DbInitializer
+	{
+		public static void Initialize(AuthDbContext context)
+		{
+			context.Database.EnsureCreated();
+		}
+	}
+}
+
---
+++ Notes.Identity/Program.cs
@@ -0,0 +1,81 @@
+﻿using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Notes.Identity;
+using Notes.Identity.Data;
+using Notes.Identity.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+var connectionString = builder.Configuration.GetValue<string>("DbConnection");
+
+services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlite(connectionString);
+});
+
+services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.Password.RequireUppercase = false;
+
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
+    .AddDeveloperSigningCredential();
+
+services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Notes.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
+
+services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AuthDbContext>();
+
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, "An error occured while app initialization");
+    }
+}
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+                    Path.Combine(app.Environment.ContentRootPath, "Styles")),
+    RequestPath = "/styles"
+});
+
+app.UseRouting();
+app.UseIdentityServer();
+//app.MapGet("/", () => "Hello World!");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+
+app.Run();
